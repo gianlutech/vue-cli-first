2d4c5a9825810132e715d2285718f7ca21b5c9da
gianlutech <gianluca.ingrao95@gmail.com>
2022-02-08 09:15:09 +0100

    add title.vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <template>
   <div id="app">
+    <Title />
     <img alt="Vue logo" src="./assets/logo.png" />
     <h1>Hello World!</h1>
   </div>
 </template>
 
 <script>
+import Title from "./components/Title.vue";
+
 export default {
   name: "App",
+  components: {
+    Title: Title,
+  },
 };
 </script>
 
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
---
+++ src/components/Title.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>
+    <h1>La mia prima app con Vue CLI!!!</h1>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+
+<style>
+</style>
